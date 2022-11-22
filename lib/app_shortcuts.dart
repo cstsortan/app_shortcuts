8693e3b34d4ee2f44a6d785989b7a3fb5cff1c55
Gaurav Maurya <83112621+mauryagaurav947@users.noreply.github.com>
2022-11-22 13:38:16 +0530

    Added null safety

--- lib/app_shortcuts.dart
+++ lib/app_shortcuts.dart
@@ -1,33 +1,31 @@
-import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:app_shortcuts/utils/ios_tutorial.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
 import 'package:dia/dia.dart';
+import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AppShortcuts {
-  static const MethodChannel _channel = const MethodChannel('app_shortcuts');
+  static const MethodChannel _channel = MethodChannel('app_shortcuts');
 
-  static void _openIOSShortcutGenerator(
+  static Future<void> _openIOSShortcutGenerator(
     String link,
     String title,
     String imageUrl, {
-    String description,
-    String shareButtonDescription,
-    String addToHomescreenButtonDescription,
+    String? description,
+    String? shareButtonDescription,
+    String? addToHomescreenButtonDescription,
   }) async {
-    var html = getIosTutorialHTML(link, title, description, imageUrl,
+    final html = getIosTutorialHTML(link, title, description, imageUrl,
         shareButtonDescription: shareButtonDescription,
-        addToHomescreenButtonDescription: addToHomescreenButtonDescription);
-    var address = 'localhost';
-    var diaApp = App();
+        addToHomescreenButtonDescription: addToHomescreenButtonDescription,);
+    const address = 'localhost';
+    final diaApp = App();
     // ignore: missing_return
-    diaApp.use((ctx, next) {
-      var uri = Uri.dataFromString(html,
-          mimeType: "text/html", base64: true, encoding: Utf8Codec());
+    diaApp.use((ctx, next) async {
+      final uri = Uri.dataFromString(html,
+          mimeType: "text/html", base64: true, encoding: const Utf8Codec(),);
       ctx.response.redirect(uri);
       diaApp.close();
     });
@@ -36,14 +34,14 @@ class AppShortcuts {
         .then((value) => print('Server started!'))
         .catchError(print);
     try {
-      await launch('http://$address:8080/', forceSafariVC: false);
+      await launchUrl(Uri.parse('http://$address:8080/'));
     } catch (error) {
       print(error);
     }
   }
 
-  static void _createAndroidShortcut(
-      String id, String link, String title, String imageUrl) async {
+  static Future<void> _createAndroidShortcut(
+      String id, String link, String title, String imageUrl,) async {
     return _channel.invokeMethod('createAndroidShortcut', {
       'title': title,
       'id': id,
@@ -52,28 +50,28 @@ class AppShortcuts {
     });
   }
 
-  static void createShortcut({
+  static Future<void> createShortcut({
     /// the name of the shortcut in the home screen
-    @required String title,
+    required String title,
 
     /// the link for the shortcut to open, most likely a deep link
-    @required String link,
+    required String link,
 
     /// a string identifier used to let android identify the shortcut
-    @required String id,
+    required String id,
 
     /// an image url for the shortcut
     /// it will also be used in the tutorial page on iOS
-    @required String imageUrl,
+    required String imageUrl,
 
     /// a description used for the tutorial page on iOS
-    String description,
+    String? description,
 
     /// a description used to prompt the user to click the share button on safari
-    String shareButtonDescription,
+    String? shareButtonDescription,
 
     /// a description used to prompt the user to click on the 'Add to homescreen' button on safari
-    String addToHomescreenButtonDescription,
+    String? addToHomescreenButtonDescription,
   }) {
     if (Platform.isAndroid) {
       return _createAndroidShortcut(id, link, title, imageUrl);
